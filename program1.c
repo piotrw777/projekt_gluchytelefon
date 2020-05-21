62dde84a39dd610ee78538ccedd24caa9df9428c
piotrw777 <piotr.wasilewski2@gmail.com>
2020-05-21 09:27:36 +0200

    Program 1 wywołuje program 2 i przekazuje argumenty

--- program1.c
+++ program1.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "funkcje_pom.h"
 
 /***************
@@ -6,6 +8,18 @@
 ***************/
 
 int main(void) {
-	printf("Witam w programie1!\n\n");
-	
+	int liczba;
+	char string[10];
+	char string_wywolania[20]="./prog2.o ";
+	char *arg_wsk = string_wywolania + 10;
+
+	printf("**********\nWitam w programie1!\n**********\n");
+	printf("Podaj liczbę całkowitą: ");
+	scanf("%d", &liczba);
+	fflush(stdout);
+	printf("Podałeś liczbę %d\n",liczba);
+	sprintf(string,"%d",liczba);
+	printf("Liczba w postaci strigu to %s\n",string);
+	strcpy(arg_wsk,string);
+	system(string_wywolania);
 }
