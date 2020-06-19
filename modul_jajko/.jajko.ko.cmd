b9b1aa786e547446967721f9f65638dad884e4a2
piotrw777 <piotr.wasilewski2@gmail.com>
2020-06-19 12:53:27 +0200

    dodano plik jajko.ko

---
+++ modul_jajko/.jajko.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/piotr/projekt_gluchytelefon/modul_jajko/jajko.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds  --build-id  -o /home/piotr/projekt_gluchytelefon/modul_jajko/jajko.ko /home/piotr/projekt_gluchytelefon/modul_jajko/jajko.o /home/piotr/projekt_gluchytelefon/modul_jajko/jajko.mod.o ;  true
---
+++ modul_jajko/.tmp_versions/jajko.mod
@@ -0,0 +1,3 @@
+/home/piotr/projekt_gluchytelefon/modul_jajko/jajko.ko
+/home/piotr/projekt_gluchytelefon/modul_jajko/jajko.o
+
